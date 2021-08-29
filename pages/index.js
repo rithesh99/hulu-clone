fb2d1b6c707cc798f6586cd897d53429109ddb23
rithesh99 <ritheshdavid@gmail.com>
2021-08-29 20:02:02 +0530

    Navbar finished

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
       <Header/>
       {/* Header */}
-
+      <Nav/>
       {/* Nav */}
 
       {/* Results */}
